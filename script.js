62e78f7b2b499db0632c8300723f434c847fe0ef
nolangh <nolanghiggins@gmail.com>
2022-05-05 20:41:52 -0600

    -a

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const socket = io("http://localhost:3000");
 
+const messageContainer = document.getElementById("message-constainer");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
+
+const name = prompt("What is your name?");
+appendMessage = "Welcome to the party";
+socket.emit("new-user", name);
 
 socket.on("chat-message", (data) => {
 	console.log(data);
 });
 
+socket.on("user-message", (name) => {
+	appendMessage(`${name} is here to party`);
+});
+
 messageForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-
 	const message = messageInput.value;
 	socket.emit("send-chat-message", message);
 	messageInput.value = "";
 });
+
+function addMessage(message) {
+	const messageElement = document.createElement("div");
+	messageElement.innerText = message;
+	messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const io = require("socket.io")(3000);
+const users = {};
 
 io.on("connection", (socket) => {
-	socket.emit("chat-message", "Hello world"); //change this to emit the users name
-
+	socket.on("new-user", (name) => {
+		users[socket.id] = name;
+		socket.broadcast.emit("user-connected", name);
+	});
 	socket.on("send-chat-message", (message) => {
+		socket.broadcast.emit("chat-message", {
+			message: message,
+			name: user[socket.id],
+		});
 		console.log(message);
 	});
 });
