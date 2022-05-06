182072e307158837c3e9bea370f29aef1afbbed6
nolangh <nolanghiggins@gmail.com>
2022-05-05 18:29:09 -0600

    -a

--- js/server.js
+++ js/server.js
@@ -1,18 +1,7 @@
-/* ----------------------- ANCHOR Port and file paths ----------------------- */
-//const express = require("express");
-// const dataFile = "chatLog.txt";
-/* ---------------------------- ANCHOR Libraries ---------------------------- */
-//const port = process.env.PORT || 8080;
-//const app = express();
-//const fs = require("fs");
 const io = require("socket.io")(3000);
 
-// app.get("/", (req, res) => {
-// 	res.send(`Server active on: ${port}`);
-// 	console.log(`Server active on: ${port}`);
-// });
-
 io.on("connection", (socket) => {
+	console.log(test);
 	socket.emit("chat-message", "Hello world"); //change this to emit the users name
 });
 
